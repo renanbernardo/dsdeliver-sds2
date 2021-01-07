42cac249507d203df19a19a9b3547bcf5611e246
Jaú Serve <renan.brnrd@gmail.com>
2021-01-07 17:19:24 -0300

    Component Hello criado

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Hello from './Hello';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Hello/>
     </div>
   );
 }
---
+++ front-web/src/Hello.tsx
@@ -0,0 +1,7 @@
+function Hello() {
+    return (
+        <h1>Componente Hello!</h1>
+    )
+}
+
+export default Hello;
