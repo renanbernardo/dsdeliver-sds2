dd42274420dc25422e83b2dab273bda5a2c325fd
Jaú Serve <renan.brnrd@gmail.com>
2021-01-08 15:53:42 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar/index';
 
 function App() {
   return (
-    <div>   
+    <>   
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
