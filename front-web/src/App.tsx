f46178d7d261f71698720e08bcef825346a1e860
Jaú Serve <renan.brnrd@gmail.com>
2021-01-08 13:42:23 -0300

    Component Counter criado

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Hello from './Hello';
+import Counter from './Counter';
 
 function App() {
   return (
-    <div>
-      <Hello/>
+    <div>   
+      <Counter />  
     </div>
   );
 }
--- front-web/src/Hello.tsx
+++
@@ -1,7 +0,0 @@
-function Hello() {
-    return (
-        <h1>Componente Hello!</h1>
-    )
-}
-
-export default Hello;
