5ba2efe2310eea1d1322dac209493ab85a42c4f8
Jaú Serve <renan.brnrd@gmail.com>
2021-01-13 14:21:13 -0300

    Add Home page

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
+import Header from './src/Header';
+import Home from './src/Home';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -17,8 +18,9 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Header />
       <StatusBar style="auto" />
+      <Header />
+      <Home />
     </View>
   );
 }
