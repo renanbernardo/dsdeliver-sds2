1b31955710661a23dcd1d54006bfc77db9b0d946
Jaú Serve <renan.brnrd@gmail.com>
2021-01-13 16:24:30 -0300

    Add Orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
-import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -19,8 +18,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      <Routes />
     </View>
   );
 }
